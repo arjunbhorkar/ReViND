ca9f40a9f0d85e7f5fdc847c7fb8869909a36297
arjunbhorkar <arjunbhorkar9@gmail.com>
2022-06-19 21:20:27 -0700

    initial cleanup

--- evaluation_recon.py
+++ evaluation_recon.py
@@ -4,33 +4,26 @@ import flax.linen as nn
 import gym
 import numpy as np
 import cv2
-# from dm_control.utils.transformations import quat_to_euler
-# from mujoco_offline_navigation.env_utils import make_dmc_env
 import matplotlib.pyplot as plt
-import np_utils
-
+import jaxrl2.np_utils as np_utils
 from matplotlib import image
-
-from jaxrl2.dataset_utils import MujImageDataset, ImageBatch, ConcatImgDataset, ReconImageDataset
-
+# from jaxrl2.dataset_utils import MujImageDataset, ImageBatch, ConcatImgDataset, ReconImageDataset
 import warnings
-warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-"I0203 23:49:00.898828 140181495007040 environment.py:135] The current episode has been running for 10000 steps."
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
 def rectify_and_resize(image, shape, rectify=False):
     if rectify:
         # jackal camera intrinsics
         fx, fy, cx, cy = 272.547000, 266.358000, 320.000000, 240.000000
-        K = np.array([[fx, 0., cx],
-                      [0., fy, cy],
-                      [0., 0., 1.]])
+        K = np.array([[fx, 0., cx], [0., fy, cy], [0., 0., 1.]])
         D = np.array([[-0.038483, -0.010456, 0.003930, -0.001007]]).T
         balance = 0.5
 
         if len(image.shape) == 4:
-            return np.array([rectify_and_resize(im_i, shape) for im_i in image])
+            return np.array(
+                [rectify_and_resize(im_i, shape) for im_i in image])
 
         image = np_utils.imrectify_fisheye(image, K, D, balance=balance)
 
@@ -40,33 +33,23 @@ def rectify_and_resize(image, shape, rectify=False):
 
 
 def project_points(xy):
-    """
-    :param xy: [batch_size, horizon, 2]
-    :return: [batch_size, horizon, 2]
-    """
     batch_size, horizon, _ = xy.shape
 
     fx, fy, cx, cy = 272.547000, 266.358000, 320.000000, 220.000000
 
     # camera is ~0.35m above ground
-    xyz = np.concatenate(
-        [xy, -0.95 * np.ones(list(xy.shape[:-1]) + [1])], axis=-1
-    )  # -0.35
+    xyz = np.concatenate([xy, -0.95 * np.ones(list(xy.shape[:-1]) + [1])],
+                         axis=-1)  # -0.35
     rvec = tvec = (0, 0, 0)
     camera_matrix = np.array([[fx, 0.0, cx], [0.0, fy, cy], [0.0, 0.0, 1.0]])
     k1, k2, p1, p2 = [-0.038483, -0.010456, 0.003930, -0.001007]
     k3 = k4 = k5 = k6 = 0.0
     dist_coeffs = (k1, k2, p1, p2, k3, k4, k5, k6)
 
-    # x = y
-    # y = -z
-    # z = x
-    xyz[..., 0] += 0.45  # NOTE(greg): shift to be in front of image plane
+    xyz[..., 0] += 0.45
     xyz_cv = np.stack([xyz[..., 1], -xyz[..., 2], xyz[..., 0]], axis=-1)
-    uv, _ = cv2.projectPoints(
-        xyz_cv.reshape(batch_size * horizon,
-                       3), rvec, tvec, camera_matrix, dist_coeffs
-    )
+    uv, _ = cv2.projectPoints(xyz_cv.reshape(batch_size * horizon, 3), rvec,
+                              tvec, camera_matrix, dist_coeffs)
     uv = uv.reshape(batch_size, horizon, 2)
 
     return uv
@@ -88,20 +71,17 @@ def evaluate(agent, batch, dir, istraj=True) -> Dict[str, float]:
             pred = []
             actual = []
             for j in range(0, len(pred_action), 2):
-                pred.append([pred_action[j+0], pred_action[j+1]])
-                actual.append([actual_waypoints[j+0], actual_waypoints[j+1]])
+                pred.append([pred_action[j + 0], pred_action[j + 1]])
+                actual.append(
+                    [actual_waypoints[j + 0], actual_waypoints[j + 1]])
             pts = np.array([pred, actual])
             print("horizon shape is", pts.shape)
             uv = project_points(pts)
-            # uv = pts
             pred = uv[0]
             real = uv[1]
 
             img = rectify_and_resize(img, (480, 640, 3))
-            # img = np.flipud(img)
             plt.clf()
-            # plt.xlim(0,640)
-            # plt.ylim(0,480)
             x, y = zip(*pred)
             x1, y1 = zip(*real)
             plt.plot(x, y, color='red')
--- train_offline_pixels_recon.py
+++ train_offline_pixels_recon.py
@@ -1,12 +1,9 @@
 #! /usr/bin/env python
-import random
 import numpy as np
-import time
 from os.path import isfile, join
 from evaluation_recon import evaluate
-from jaxrl2.dataset_utils import MujImageDataset, ImageBatch, ConcatImgDataset, ReconImageDataset, format_filter
+from jaxrl2.dataset_utils import ReconImageDataset
 from jaxrl2.agents import PixelIQLLearner
-import pickle
 import os
 import tensorflow as tf
 import tqdm
@@ -14,18 +11,13 @@ from absl import app, flags
 from flax.metrics.tensorboard import SummaryWriter
 from ml_collections import config_flags
 from flax.training import checkpoints
-# from collect_data import collect
-import gc
 
 tf.config.experimental.set_visible_devices([], "GPU")
 
-
-fine_tine_iterations = 3
-
 FLAGS = flags.FLAGS
 
-flags.DEFINE_string('env_name', 'mujoco_car_dcist', 'Environment name.')
-flags.DEFINE_string('save_dir', './grace_recon3/', 'Tensorboard logging dir.')
+flags.DEFINE_string('exp_dir', 'experiments', 'Environment name.')
+flags.DEFINE_string('save_dir', './tensorboard/', 'Tensorboard logging dir.')
 flags.DEFINE_integer('seed', 42, 'Random seed.')
 flags.DEFINE_integer('eval_episodes', 10,
                      'Number of episodes used for evaluation.')
@@ -33,19 +25,7 @@ flags.DEFINE_integer('log_interval', 300, 'Logging interval.')
 flags.DEFINE_integer('eval_interval', 15000, 'Eval interval.')
 flags.DEFINE_integer('batch_size', 512, 'Mini batch size.')
 flags.DEFINE_integer('max_steps', int(2000000), 'Number of training steps.')
-flags.DEFINE_integer('start_training', int(1e3),
-                     'Number of training steps to start training.')
-flags.DEFINE_integer('data_pull_interval', int(2000000),
-                     'Number of episodes used for evaluation.')
-flags.DEFINE_integer('image_size', 64, 'Image size.')
-flags.DEFINE_integer('num_stack', 3, 'Stack frames.')
-flags.DEFINE_integer('replay_buffer_size', None,
-                     'Number of training steps to start training.')
-flags.DEFINE_integer(
-    'action_repeat', None,
-    'Action repeat, if None, uses 2 or PlaNet default values.')
 flags.DEFINE_boolean('tqdm', True, 'Use tqdm progress bar.')
-flags.DEFINE_boolean('save_video', False, 'Save videos during evaluation.')
 config_flags.DEFINE_config_file(
     'config',
     'configs/offline_pixels_default.py',
@@ -54,38 +34,18 @@ config_flags.DEFINE_config_file(
 
 
 def main(_):
-    name = f'IQL_collision_herfinal_updatedn50_Bimp_HER_lr{FLAGS.config.actor_lr}_expectile{FLAGS.config.expectile}_Ascale{FLAGS.config.A_scaling}'
-    summary_writer = SummaryWriter(os.path.join(FLAGS.save_dir, name))
+    name = f'IQL_grassy{FLAGS.config.actor_lr}_expectile{FLAGS.config.expectile}_Ascale{FLAGS.config.A_scaling}'
+    summary_writer = SummaryWriter(
+        os.path.join(FLAGS.exp_dir, FLAGS.save_dir, name))
 
-    images_file_path = './recon_graphs_waypoints/' + name + '/'
+    images_file_path = f'./{FLAGS.exp_dir}/recon_graphs_waypoints/' + name + '/'
 
-    # bag_dir = "/nfs/kun2/users/arjun/pklcollision2"
-    # Good traj ac space newer
-    # bag_dir = "/home/arjunbhorkar/pkl_bright1"
-
-    # vel ac space
-    bag_dir = "/nfs/kun2/users/arjun/pkl_bright_newacs_avg"
-
-    bag_list = [str(bag_dir + '/' + f) for f in os.listdir(bag_dir)
-                if isfile(join(bag_dir, f)) and "traj" in f]
-    # new_dir = random.choice(bag_list)
     new_dir = "/nfs/kun2/users/arjun/recon_dataset/recon_datavis/pklher3/traj_train.pkl"
     print("Using training dataset: ", new_dir)
 
-    # FC
-
-    # t = format_filter(new_dir, True)
-    # dataset1 = ReconImageDataset(t, isdir=False)
-
-    # FC
-    # notfc
-
     dataset1 = ReconImageDataset(new_dir, isher=True)
 
-    # NOTFC
-
     samp_a = dataset1.sample(1, isher=True)
-
     print(samp_a.observations[0][np.newaxis].shape)
     print(samp_a.image_observations[0][np.newaxis].shape)
     print(samp_a.actions[0][np.newaxis].shape)
@@ -101,45 +61,39 @@ def main(_):
                        smoothing=0.1,
                        disable=not FLAGS.tqdm):
 
-        # , not_rand=True, saveimg=True)
         batch = dataset1.sample(FLAGS.batch_size, saveimg=False, isher=True)
         update_info = agent.update(batch)
-
-        if i % FLAGS.data_pull_interval == 0:
-            new_dir = random.choice(bag_list)
-            print("Using dataset: ", new_dir)
-
-            dataset1 = ReconImageDataset(new_dir)
 
         if i % FLAGS.log_interval == 0:
             for k, v in update_info.items():
                 if v.ndim == 0:
-                    summary_writer.scalar(f'training/{k}', v,
-                                          i)
+                    summary_writer.scalar(f'training/{k}', v, i)
                 else:
-                    summary_writer.histogram(f'training/{k}', v,
-                                             i)
+                    summary_writer.histogram(f'training/{k}', v, i)
             summary_writer.flush()
 
         if i % FLAGS.eval_interval == 0 or i == 100 or i == 20000:
             eval_batch = dataset1.sample(7000, isher=True)
 
-            eval_info = evaluate(
-                agent, eval_batch, images_file_path+f'step{i}', True)
+            eval_info = evaluate(agent, eval_batch,
+                                 images_file_path + f'step{i}', True)
 
-            # running_reward += rewards
             for k, v in eval_info.items():
                 summary_writer.scalar(f'evaluation/{k}', v, i)
-            # summary_writer.scalar(f'evaluation/running rewards average', np.mean(running_reward), i)
-            # summary_writer.histogram(f'evaluation/running rewards', running_reward, i)
-
-            policy_folder = os.path.join('policies_off_exp_5', name)
+            policy_folder = os.path.join(f'{FLAGS.exp_dir}/model_checkpoints',
+                                         name)
             os.makedirs(policy_folder, exist_ok=True)
 
-            param_dict = {"actor": agent._actor, "critic": agent._critic,
-                          "value": agent._value, "target_critic_params": agent._target_critic_params}
-            checkpoints.save_checkpoint(
-                policy_folder, param_dict, step=i, keep=1000)
+            param_dict = {
+                "actor": agent._actor,
+                "critic": agent._critic,
+                "value": agent._value,
+                "target_critic_params": agent._target_critic_params
+            }
+            checkpoints.save_checkpoint(policy_folder,
+                                        param_dict,
+                                        step=i,
+                                        keep=1000)
 
 
 if __name__ == '__main__':
